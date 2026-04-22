0b1316bebf6f66bd9881eb5e623e1e747c9e6bf4
abhaythakur2005 <iadesanya440@gmail.com>
2026-04-22 22:09:55 +0530

    Initial skill swap project

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import { errorHandler, notFoundHandler } from "./middleware/errorHandler.js";
 
 // Load environment variables
 dotenv.config();
+console.log("ENV CHECK:", process.env.MONGODB_URI);
 
 // Initialize Express App
 const app = express();
